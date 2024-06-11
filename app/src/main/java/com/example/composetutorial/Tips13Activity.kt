ead4014580b2cc865c8628062db92ccac840168c
est7 <est7@qq.com>
2024-06-11 12:34:40 +0800

    feat: 为了测试 xml 跟 compose 的生命周期互联 加入 activity 包

---
+++ app/src/main/java/com/example/composetutorial/Tips13Activity.kt
@@ -0,0 +1,27 @@
+package com.example.composetutorial
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.composetutorial.presentation.feature.tips_13.Tips13Fragment
+
+class Tips13Activity : AppCompatActivity() {
+    @SuppressLint("CommitTransaction")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tips13)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //add tips13Fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, Tips13Fragment())
+            .commit()
+    }
+}
